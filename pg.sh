f69418fea69547d92cec13a94aa4995b2d7adfc7
anand <anand@rathnas.com>
2024-03-12 02:12:46 -0700

    [skip ci] cleanup

--- pg.sh
+++ pg.sh
@@ -4,10 +4,7 @@
 echo "Enter album name: "
 read tmpAlbumName
 albumName="PG${tmpAlbumName//[^[:alnum:]]/}"
-echo "Album name (clensed): $albumName"
-
-# Prompt for confirmation to proceed
-echo "Proceed with the above album name? (Y/n)"
+echo "Album name (clensed): $albumName, proceed? (Y/n)"
 read proceed
 if [ "$proceed" = "n" ]; then
   echo "Exiting..."
@@ -16,20 +13,28 @@ fi
 
 # Proceed only if git config user.name and user.email are set
 if [ -z "$(git config user.name)" ]; then
-  echo "git config user.name is not set. Exiting..."
-  exit 1
+  echo "git config user.name is not set. Retry after setting it like..."
+  echo "git config user.name yourname"
+  exit 2
 fi
 if [ -z "$(git config user.email)" ]; then
-  echo "git config user.email is not set. Exiting..."
-  exit 1
+  echo "git config user.email is not set. Retry after setting it like..."
+  echo "git config user.email yourname@example.com"
+  exit 3
 fi
+# Proceed only if gh is installed
+if ! command -v gh &> /dev/null; then
+  echo "gh is not installed. Go to cli.github.com to install. Exiting..."
+  exit 4
+fi
+
 
 
 
 # ~~
 
 # rename to $albumName (prefix with PG and only alphanumeric characters)
-mv ../pixel-geeks ../$albumName
+mv ../pixel-geeks "$albumName"
 
 # Replace all pixel-geeks in the album with $albumName
 sed -i '' -e "s/pixel-geeks/$albumName/g" package.json
@@ -41,13 +46,12 @@ sed -i '' -e "s/pixel-geeks/$tmpAlbumName/g" ./setup/digest.js
 # ~~
 
 # prompt for a directory where the photos are located
-echo "Enter directory where photos are located: "
-read dir
+read -e -p "Enter directory where photos are located: " dir
 
 # Proceed only if the directory exists
 if [ ! -d "$dir" ]; then
-  echo "Directory does not exist. Exiting..."
-  exit 1
+  echo "Directory "$dir" does not exist. Exiting..."
+  exit 5
 fi
 
 # copy all the photos from $dir to ./setup/input
@@ -57,7 +61,7 @@ cp $dir/* ./setup/input
 # Proceed only if ./setup/input is not empty
 if [ ! "$(ls -A ./setup/input)" ]; then
   echo "No photos found in setup/input. Exiting..."
-  exit 1
+  exit 6
 fi
 
 # ~~ 
@@ -77,7 +81,30 @@ updatedRemoteUrl=$(echo "$remoteUrl" | sed "s/pixel-geeks/$albumName/g")
 echo -e "Updated remote repo url from: $remoteUrl to: $updatedRemoteUrl"
 git remote set-url origin $updatedRemoteUrl
 
+# Remove username and token from $updatedRemoteUrl
+updatedRemoteUrlNoCreds=$(echo "$updatedRemoteUrl" | sed "s/https:\/\/.*@/https:\/\//")
 
+
+
+# ~~
+
+# Create a new private repository on GitHub
+tmp=${updatedRemoteUrlNoCreds#https://github.com/}
+tmp=${tmp%.git}
+if gh api /orgs/"$usernameOrOrgname" &> /dev/null; then
+  orgname=$(echo "$tmp" | cut -d'/' -f1)
+  gh repo create "$tmp" --private --homepage "https://$orgname.github.io/$albumName/"
+  echo "Organization found. Creating repository $tmp"
+else
+  username=$(echo "$tmp" | cut -d'/' -f1)
+  gh repo create "$albumName" --private --homepage "https://$username.github.io/$albumName/"
+  echo "Creating repository $tmp"
+fi
+
+echo "Repository created. Waiting for 5 seconds..."
+sleep 5
+
+echo "Pushing to the new repo $updatedRemoteUrlNoCreds"
 # Commit and push to the new repository
 git config user.name
 git config user.email
@@ -88,9 +115,6 @@ git push -u origin main
 
 
 # ~~
-
-# Remove username and token from $updatedRemoteUrl
-updatedRemoteUrlNoCreds=$(echo "$updatedRemoteUrl" | sed "s/https:\/\/.*@/https:\/\//")
 
 # Open the new repository in the browser
 open "$updatedRemoteUrlNoCreds"
